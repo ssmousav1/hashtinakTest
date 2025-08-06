c8737efbff0aae87b8ddb5d3c9742c68b2cb020f
ssmousav1 <sobhanmousavi121122@gmail.com>
2025-08-06 13:42:20 +0330

    update lint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -13,4 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+export default eslintConfig([
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    },
+  },
+]);
